e540ea0c0c3c7ad8ef8a7873ad07c798363950ab
NicolasPrates <nicolaspratesaraujo@gmail.com>
2022-04-13 20:34:25 -0300

    Initial commit

---
+++ src/main/java/com/nicpratt/dt_money_backend/DtMoneyBackendApplication.java
@@ -0,0 +1,13 @@
+package com.nicpratt.dt_money_backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DtMoneyBackendApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DtMoneyBackendApplication.class, args);
+    }
+
+}
